ae858b041fc04e1dcbea01140ae9191d80a09c1a
bjarkiella <bjarkiella@gmail.com>
2024-12-07 22:18:33 +0100

    Basic layout set, just missing the two extra rules

---
+++ LegoWall/src/BrickRules.java
@@ -0,0 +1,10 @@
+public class BrickRules {
+
+    // Rule A: layerSum from ApplyBricks
+
+    // Rule B: No straight lines in two consecutive layers
+        // Return True if ok, else re-do line?
+    // Rule C: Hole in every line
+        // Select one of the bricks and set to 0
+        // Check with rule B if ok?
+}
